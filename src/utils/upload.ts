1662f43afd7c3b5a214a5eb3cafe302cc1e5cc4e
Kittiwat Choocheep <KyouKung.000@gmail.com>
2023-09-04 10:22:40 +0700

    feat:file upload

---
+++ src/utils/upload.ts
@@ -0,0 +1,32 @@
+import { supabase } from "./db";
+import fs from "fs/promises";
+
+const supabaseUpload = async (files: any) => {
+  const fileUrls = [];
+
+  for (let file of files) {
+    try {
+      const { data, error } = await supabase.storage
+        .from("user-storage")
+        .upload(`avatar_${Date.now()}`, files);
+
+      if (error) {
+        console.error("Supabase error: ", error.message);
+        continue;
+      }
+
+      // บันทึก URL จากการอัปโหลด
+      if (data) {
+        //@ts-ignore
+        fileUrls.push(data.Key);
+      }
+      await fs.unlink(file.path);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  }
+
+  return fileUrls;
+};
+
+export { supabaseUpload };
