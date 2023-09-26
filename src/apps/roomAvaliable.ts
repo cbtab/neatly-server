53037f5fd6faaa2714f04f10461214f1252146f1
Kittiwat Choocheep <KyouKung.000@gmail.com>
2023-09-26 16:45:01 +0700

    feat: add avaliable api

--- src/apps/roomAvaliable.ts
+++ src/apps/roomAvaliable.ts
@@ -158,3 +158,24 @@ roomAvaliable.get("/:roomId", async (req, res) => {
     console.log(error);
   }
 });
+
+roomAvaliable.get("/admin/admin", async (req: Request, res: Response) => {
+  try {
+    const { data, error } = await supabase
+      .from("room_avaliable")
+      .select("*, room_details(room_type, bed_types)")
+      .order("room_avaliable_id", { ascending: true });
+
+    if (error) {
+      console.error("Error fetching room avaliable:", error);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while fetching room avaliable." });
+    }
+
+    res.json(data);
+  } catch (err) {
+    console.error("Internal server error:", err);
+    res.status(500).json({ error: "An internal server error occurred." });
+  }
+});
